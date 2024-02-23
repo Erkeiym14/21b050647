9a914238c15589e5144e78678933e5fefd8eba38
Erkeiym14 <144516954+Erkeiym14@users.noreply.github.com>
2024-02-24 00:06:07 +0600

    lab4

---
+++ lab4/generators/4.py
@@ -0,0 +1,16 @@
+def squares():
+    i = 1
+    while True:
+        yield i*i
+        i += 1 
+
+a = int(input())
+b = int(input())
+for num in squares():
+    if num in range(a, b):
+        print(num)
+     
+    
+n=int(input())
+a=range(n)
+print(list(a)[::-1])
